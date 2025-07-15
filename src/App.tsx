7ac4f55535f0e853c70377d294526af9fe64e167
Jonathan Wolff <jwolff@isentropy.com>
2025-07-15 23:07:22 +0200

    favicon etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserProvider, JsonRpcProvider, parseEther, Provider, Signer, toBeHex, WeiPerEther } from 'ethers';
-import { CloneFactory__factory, OwnedBrevityInterpreter, OwnedBrevityInterpreter__factory } from '@isentropy/brevity-lang/typechain-types';
+import { BrowserProvider, Signer } from 'ethers';
 import { useSDK } from '@metamask/sdk-react';
 import BlockExplorerLink from './BlockExplorerLink';
-import BrevityInterpreterStats from './BrevityInterpreterStats';
-import Runner from './Runner';
-import { Script } from 'vm';
-import ScriptSelector, { ScriptAndDesc } from './ScriptSelector';
 import Interpreter from './Interpreter'
-import { ADDRESSES_BYCHAINID, scriptsFromChainId } from './templateScripts';
 import ConnectOrCreate from './ConnectOrCreate';
 export const QUERY_PARAM_BREVITY_ADDRESS = 'b'
-//0x00c36BbbF0151280d052C4a3f2D3cFeB52ee1f84
-
-//const DEFAULT_BREVITY_INTERPRETER = '0xb6AA3ce7d5eAcD1a1faE7A9740a5064436946eA3'
-//const defaultProvider = new JsonRpcProvider('https://rpc.gnosischain.com/');
-//const defaultChainID = '0x64'
 
 interface Window {
   ethereum: any
@@ -29,12 +18,6 @@ function App() {
 
   const urlParams = new URLSearchParams(window.location.search);
   let interpreterAddress = urlParams.get(QUERY_PARAM_BREVITY_ADDRESS);
-  /*
-  if (!interpreterAddress) {
-    interpreterAddress = DEFAULT_BREVITY_INTERPRETER
-    window.location.search = `?${QUERY_PARAM_BREVITY_ADDRESS}=` + DEFAULT_BREVITY_INTERPRETER
-  }
-  */
   const [account, setAccount] = useState<string>();
   const [signer, setSigner] = useState<Signer>()
   const [deployed, setDeployed] = useState<boolean>(false)
@@ -60,7 +43,7 @@ function App() {
 
   return (
     <div className="brevityGui">
-      <h3> Brevity Demo. USE AT YOUR OWN RISK! See <a href='https://github.com/Isentropy/brevity/blob/master/LICENSE'>Brevity license</a>.</h3>
+      <h3> Brevity Demo. Use at your own risk! See <a href='https://github.com/Isentropy/brevity/blob/master/LICENSE'>Brevity license</a>.</h3>
       <button style={{ padding: 10, margin: 10 }} onClick={connect}>
         Connect MetaMask
       </button>
@@ -81,6 +64,12 @@ function App() {
       { account && signer && !deployed && 
         <ConnectOrCreate signer={signer} account={account}></ConnectOrCreate>
       }
+      <div><hr></hr>
+      Copyright 2024-2025 <a href="http://isentropy.com"> Isentropy LLC 
+      </a>
+      <br></br>
+      <br></br>
+      THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</div>
     </div>
   );
 }
