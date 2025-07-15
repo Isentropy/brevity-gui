6702d5d9ea21b67c06a6b8d957c9267875744066
Jonathan Wolff <jwolff@isentropy.com>
2025-07-15 21:41:53 +0200

    refac

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserProvider, JsonRpcProvider, parseEther, Provider, toBeHex, WeiPerEther } from 'ethers';
+import { BrowserProvider, JsonRpcProvider, parseEther, Provider, Signer, toBeHex, WeiPerEther } from 'ethers';
 import { CloneFactory__factory, OwnedBrevityInterpreter, OwnedBrevityInterpreter__factory } from '@isentropy/brevity-lang/typechain-types';
 import { useSDK } from '@metamask/sdk-react';
 import BlockExplorerLink from './BlockExplorerLink';
@@ -8,14 +8,14 @@ import BrevityInterpreterStats from './BrevityInterpreterStats';
 import Runner from './Runner';
 import { Script } from 'vm';
 import ScriptSelector, { ScriptAndDesc } from './ScriptSelector';
+import Interpreter from './Interpreter'
 import { ADDRESSES_BYCHAINID, scriptsFromChainId } from './templateScripts';
-const QUERY_PARAM_BREVITY_ADDRESS = 'b'
+export const QUERY_PARAM_BREVITY_ADDRESS = 'b'
 //0x00c36BbbF0151280d052C4a3f2D3cFeB52ee1f84
 
-const DEFAULT_BREVITY_INTERPRETER = '0xb6AA3ce7d5eAcD1a1faE7A9740a5064436946eA3'
-const CLONECODE = /0x363d3d373d3d3d363d73([a-f0-9]{40,40})5af43d82803e903d91602b57fd5bf3/
-const defaultProvider = new JsonRpcProvider('https://rpc.gnosischain.com/');
-const defaultChainID = '0x64'
+//const DEFAULT_BREVITY_INTERPRETER = '0xb6AA3ce7d5eAcD1a1faE7A9740a5064436946eA3'
+//const defaultProvider = new JsonRpcProvider('https://rpc.gnosischain.com/');
+//const defaultChainID = '0x64'
 
 interface Window {
   ethereum: any
@@ -28,58 +28,34 @@ function App() {
 
   const urlParams = new URLSearchParams(window.location.search);
   let interpreterAddress = urlParams.get(QUERY_PARAM_BREVITY_ADDRESS);
+  /*
   if (!interpreterAddress) {
     interpreterAddress = DEFAULT_BREVITY_INTERPRETER
     window.location.search = `?${QUERY_PARAM_BREVITY_ADDRESS}=` + DEFAULT_BREVITY_INTERPRETER
   }
-
-  const [interpreter, setInterpreter] = useState<OwnedBrevityInterpreter>(OwnedBrevityInterpreter__factory.connect(interpreterAddress, defaultProvider));
+  */
   const [account, setAccount] = useState<string>();
-  const [owner, setOwner] = useState<string>();
+  const [signer, setSigner] = useState<Signer>()
+  const [deployed, setDeployed] = useState<boolean>(false)
   const { sdk, connected, connecting, chainId, provider } = useSDK();
-  const [script, setScript] = useState<string>(scriptsFromChainId(defaultChainID)[0].script);
 
   const connect = async () => {
     try {
-      //const accounts = await sdk?.connect();
-      //console.log(`accounts ${accounts}`)
       const provider_Metamask = new BrowserProvider(window.ethereum);
-      provider_Metamask.getSigner().then((signer) => {
-        const newInterpreter = OwnedBrevityInterpreter__factory.connect(interpreterAddress!, signer)
-        setInterpreter(newInterpreter)
-        newInterpreter.owner().then((a) => {setOwner(a)})
+      if(interpreterAddress) provider_Metamask.getCode(interpreterAddress).then((code) =>{
+        if(code && code != '0x') {
+          setDeployed(true)
+        }
       })
-      const accounts2 = await provider_Metamask.send("eth_requestAccounts", []);
-
-      setAccount(accounts2?.[0]);
+      if(!signer) provider_Metamask.getSigner().then((s) => {
+        setSigner(s)
+        s.getAddress().then((a) => {setAccount(a)})
+      })
     } catch (err) {
       console.warn("failed to connect..", err);
     }
   };
-  const clone = async () => {
-    try {
-      const cloneFactory = chainId ? ADDRESSES_BYCHAINID.get(toBeHex(chainId, 32))?.cloneFactory : chainId
-      if(!cloneFactory) {
-        console.error(`No CloneFactory defined for chainId ${chainId}`)
-        return
-      }
-      const factory = CloneFactory__factory.connect(cloneFactory, interpreter.runner)
-      const salt = toBeHex(new Date().getTime(), 32)
-      // if its a proxy, clone the proxy's target not the proxy
-      const code = await interpreter.runner?.provider?.getCode(interpreterAddress!)
-      const cloneParse = CLONECODE.exec(code!)
-      const implementation = cloneParse ? '0x' + cloneParse[1] : interpreterAddress!
-      console.log(`implementation ${implementation}`)
-      const newInterpreter = await factory.predictDeterministicAddress(implementation, salt, account!)
-      factory.cloneDeterministic(interpreterAddress!, salt, account!).then((resp) => {
-        resp.wait().then((tr) => {
-          window.location.search = `?${QUERY_PARAM_BREVITY_ADDRESS}=` + newInterpreter
-        })
-      })
-    } catch (err) {
-
-    }
-  }
+  
 
   return (
     <div className="brevityGui">
@@ -88,20 +64,19 @@ function App() {
         Connect MetaMask
       </button>
       <br></br>
-      Connected to Brevity interpreter {BlockExplorerLink(interpreterAddress, chainId)} on chain {chainId}
-      <button style={{ padding: 10, margin: 10 }} disabled={account && chainId && ADDRESSES_BYCHAINID.get(toBeHex(chainId, 32))?.cloneFactory ? false : true} onClick={clone}>Clone</button>
 
       {account && (
         <div>
           <p></p>
-          Connected to MetaMask with wallet: {BlockExplorerLink(account, chainId)} {owner?.toLowerCase() == account.toLowerCase() ? "OWNER" : "NOT OWNER"}
+          Connected to MetaMask with wallet: {BlockExplorerLink(account, chainId)} on chainId {chainId}
         </div>
       )}
-      <br></br>
-      <ScriptSelector callback={setScript} scripts={scriptsFromChainId(chainId)} optionsLength={10}></ScriptSelector>
-      <br></br>
-      <Runner interpreter={interpreter} account={account} script={script} chainId={chainId}></Runner>
-      {interpreter && BrevityInterpreterStats(interpreter, chainId)}
+      { account && interpreterAddress && chainId && signer && deployed &&
+        <Interpreter interpreterAddress={interpreterAddress} signer={signer} chainId={chainId} account={account}></Interpreter>
+      }
+      { account && interpreterAddress && chainId && signer && !deployed &&
+        `Interpreter Not Deployed: ${interpreterAddress}`
+      }
     </div>
   );
 }
---
+++ src/Interpreter.tsx
@@ -0,0 +1,64 @@
+import { CloneFactory__factory, OwnedBrevityInterpreter, OwnedBrevityInterpreter__factory } from '@isentropy/brevity-lang/typechain-types';
+import { Signer, toBeHex } from "ethers"
+import BlockExplorerLink from "./BlockExplorerLink"
+import ScriptSelector from "./ScriptSelector"
+import Runner from "./Runner"
+import { useState } from 'react';
+import { ADDRESSES_BYCHAINID, scriptsFromChainId } from './templateScripts';
+import BrevityInterpreterStats from './BrevityInterpreterStats';
+import { QUERY_PARAM_BREVITY_ADDRESS } from './App';
+
+const CLONECODE = /0x363d3d373d3d3d363d73([a-f0-9]{40,40})5af43d82803e903d91602b57fd5bf3/
+
+interface Props {
+    interpreterAddress: string
+    signer: Signer
+    chainId: string
+    account: string
+}
+//const [interpreter, setInterpreter] = useState<OwnedBrevityInterpreter>();
+
+function Interpreter(p: Props) {
+    const [script, setScript] = useState<string>();
+    const [owner, setOwner] = useState<string>();
+
+    const interpreter = OwnedBrevityInterpreter__factory.connect(p.interpreterAddress, p.signer)
+    if(!script) setScript(scriptsFromChainId(p.chainId.toString())[0].script)
+    if(!owner) interpreter.owner().then((o) => {setOwner(o)})
+    const clone = async () => {
+        try {
+            const cloneFactory = p.chainId ? ADDRESSES_BYCHAINID.get(toBeHex(p.chainId, 32))?.cloneFactory : p.chainId
+            if (!cloneFactory) {
+                console.error(`No CloneFactory defined for chainId ${p.chainId}`)
+                return
+            }
+            const factory = CloneFactory__factory.connect(cloneFactory, interpreter.runner)
+            const salt = toBeHex(new Date().getTime(), 32)
+            // if its a proxy, clone the proxy's target not the proxy
+            const code = await interpreter.runner?.provider?.getCode(p.interpreterAddress)
+            const cloneParse = CLONECODE.exec(code!)
+            const implementation = cloneParse ? '0x' + cloneParse[1] : p.interpreterAddress
+            console.log(`implementation ${implementation}`)
+            const newInterpreter = await factory.predictDeterministicAddress(implementation, salt, p.account)
+            factory.cloneDeterministic(p.interpreterAddress, salt, p.account).then((resp) => {
+                resp.wait().then((tr) => {
+                    window.location.search = `?${QUERY_PARAM_BREVITY_ADDRESS}=` + newInterpreter
+                })
+            })
+        } catch (err) {
+
+        }
+    }
+    return <div>
+        Connected to Brevity interpreter {BlockExplorerLink(p.interpreterAddress, p.chainId)} as {owner?.toLowerCase() == p.account.toLowerCase() ? "OWNER" : "NOT OWNER"}
+        <button style={{ padding: 10, margin: 10 }} disabled={ADDRESSES_BYCHAINID.get(toBeHex(p.chainId, 32))?.cloneFactory ? false : true} onClick={clone}>Clone</button>
+
+        <br></br>
+        <ScriptSelector callback={setScript} scripts={scriptsFromChainId(p.chainId)} optionsLength={10}></ScriptSelector>
+        <br></br>
+        <Runner interpreter={interpreter} account={p.account} script={script} chainId={p.chainId}></Runner>
+        {interpreter && BrevityInterpreterStats(interpreter, p.chainId)}
+    </div>
+}
+
+export default Interpreter
